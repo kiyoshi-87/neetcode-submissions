b0a0cf8913f793f96ab9809a57c394d86f3cbed6
Ayan Sarma <ayansarmajrht@gmail.com>
2026-07-20 16:36:17 +0530

    Add: majority-element-ii - submission-2

---
+++ Data Structures & Algorithms/majority-element-ii/submission-2.java
@@ -0,0 +1,60 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+        int candidate1 = 0;
+        int candidate2 = 0;
+
+        int count1 = 0;
+        int count2 = 0;
+
+        // First pass: Find possible candidates
+        for (int num : nums) {
+            if (candidate1 == num) {
+                count1++;
+            }
+
+            else if (candidate2 == num) {
+                count2++;
+            }
+
+            else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            }
+
+            else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            }
+
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Second pass: Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1)
+                count1++;
+
+            else if (num == candidate2)
+                count2++;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        int limit = nums.length / 3;
+
+        if (count1 > limit)
+            ans.add(candidate1);
+
+        if (count2 > limit)
+            ans.add(candidate2);
+
+        return ans;
+    }
+}
